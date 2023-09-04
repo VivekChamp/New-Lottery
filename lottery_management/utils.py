62d2001a2fe25146c337f8525af0aa839ee4ad90
vivekchamp84 <vivekchamp84@gmail.com>
2023-09-04 12:01:58 +0000

    Barcode Correction

--- lottery_management/utils.py
+++ lottery_management/utils.py
@@ -1,11 +1,10 @@
 import frappe
 
 @frappe.whitelist()
-def get_bundle_details(barcode):
-    print("get_bundle_details function called")  # Check if the function is being called
-    bundle = frappe.get_doc('Ticket Bundle', {'barcode': barcode})
-    print("Bundle:", bundle)  # Check if the bundle is fetched correctly
-    if bundle:
-        return {'bundle_number': bundle.bundle_number}
+def get_bundle_details(barcode):    
+    bundle_list = frappe.get_all("Ticket Bundle", {"bundle_number": barcode[:10], "start_sequence": ["<=", barcode[10:13]], "end_sequence": [">=", barcode[10:13]]}, ["bundle_number"])
+    
+    if bundle_list:
+        return {'bundle_number': bundle_list[0]["bundle_number"]}
+    
     return None
-
